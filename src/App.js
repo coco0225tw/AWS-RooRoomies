cc1ce4bf10b54ab6f2f9d8d979d33dbfffc1a74e
River Kuo <coco0958231520@gmail.com>
2022-08-31 21:57:16 +0800

    install firebase and deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { initializeApp } from 'firebase/app';
+import { query, getFirestore, getDocs, collection } from 'firebase/firestore';
+import React, { useEffect } from 'react';
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxZxLUfOcXF0TTHQr7QJlOmtFNUhH_w2Q',
+  authDomain: 'rooroomies.firebaseapp.com',
+  projectId: 'rooroomies',
+  storageBucket: 'rooroomies.appspot.com',
+  messagingSenderId: '902090494840',
+  appId: '1:902090494840:web:b89eee21700f2fb39e2e8d',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
+  useEffect(() => {
+    // async function getDoc() {
+    //   const q = query(collection(db, 'users'));
+    //   const querySnapshot = await getDocs(q);
+    //   querySnapshot.forEach((doc) => {
+    //     console.log(doc.id, ' => ', doc.data());
+    //   });
+    // }
+    // getDoc();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>successfully build</p>
       </header>
     </div>
   );
