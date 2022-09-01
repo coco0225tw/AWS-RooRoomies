77e758f072a48f6ca48353a732d841d7e7757058
River Kuo <coco0958231520@gmail.com>
2022-09-01 23:55:52 +0800

    myListing component

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import firebase from '../../utils/firebase';
+import MyListing from './MyListing';
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -9,70 +10,10 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 `;
-
-const Title = styled.div``;
-
-const UploadMainImage = styled.input.attrs({
-  type: 'file',
-  accept: 'image/*',
-})``;
-const UploadImages = styled.input.attrs({
-  type: 'file',
-  multiple: true,
-})``;
-const PreviewMainImage = styled.img`
-  src: (${(props) => props.src});
-  width: 30vw;
-`;
-
-const PreviewImages = styled(PreviewMainImage)``;
-const SubmitBtn = styled.div`
-  padding: 10px;
-`;
 function Profile() {
-  const [mainImgUrl, setMainImgUrl] = useState<string>();
-  const [imagesUrl, setImagesUrl] = useState<string[]>();
-  const [imagesBlob, setImagesBlob] = useState<Blob[]>();
-  const [mainImgBlob, setMainImgBlob] = useState<Blob>();
-
-  function previewMainImage(e: React.ChangeEvent<HTMLInputElement>) {
-    let target = e.target as HTMLInputElement;
-    let files = target.files;
-    if ((files as FileList)[0]) {
-      setMainImgUrl(URL.createObjectURL((files as FileList)[0]));
-      setMainImgBlob((files as FileList)[0]);
-    }
-  }
-
-  function previewImages(e: React.ChangeEvent<HTMLInputElement>) {
-    let target = e.target as HTMLInputElement;
-    let files = target.files;
-    function loopImages(files: FileList) {
-      const fl = files.length;
-      let arr = [];
-      let blobArr = [];
-      for (let i = 0; i < fl; i++) {
-        let file = files[i];
-        arr.push(URL.createObjectURL(file));
-        blobArr.push(file);
-      }
-      setImagesUrl(arr);
-      setImagesBlob(blobArr);
-    }
-    loopImages(files!);
-  }
-  async function uploadAllImages() {
-    await firebase.uploadMainImage(mainImgBlob!);
-    await firebase.uploadImages(imagesBlob as []);
-  }
   return (
     <Wrapper>
-      <Title>上傳圖片</Title>
-      <UploadMainImage onChange={(e) => previewMainImage(e)} />
-      {mainImgUrl && <PreviewMainImage src={mainImgUrl as string} />}
-      <UploadImages onChange={(e) => previewImages(e)} />
-      {imagesUrl && imagesUrl.map((url, index) => <PreviewImages key={`previewImages${index}`} src={url as string} />)}
-      <SubmitBtn onClick={uploadAllImages}>送出</SubmitBtn>
+      <MyListing />
     </Wrapper>
   );
 }
