fba6b99d0b0c5a8255f8bdab06e60190f4333b6c
River Kuo <coco0958231520@gmail.com>
2022-09-16 17:14:54 +0800

    import logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
@@ -13,6 +14,13 @@ const Wrapper = styled.div`
 const Title = styled.div`
   font-size: 30px;
 `;
+const SectionWrapper = styled.div``;
+const LogoImg = styled.div``;
+const LogoText = styled.div``;
+const NavBtn = styled.div``;
+const SignInBtn = styled(Link);
+const LogoutBtn = styled.div``;
+// const Notification = styled.div``;
 function Header() {
   return (
     <Wrapper>
