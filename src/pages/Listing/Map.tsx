4eefaaad0d89c5ba81d2678c81cded576f09c927
River Kuo <coco0958231520@gmail.com>
2022-09-16 09:14:49 +0800

    complete fetch cloud function server

--- src/pages/Listing/Map.tsx
+++ src/pages/Listing/Map.tsx
@@ -1,6 +1,125 @@
 import { GoogleMap, useJsApiLoader, useLoadScript, Marker } from '@react-google-maps/api';
 import React, { useState, useRef, useMemo, useEffect } from 'react';
 import GoogleMapKey from '../../key';
+const style = [
+  {
+    featureType: 'administrative',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+      {
+        hue: '#0066ff',
+      },
+      {
+        saturation: 74,
+      },
+      {
+        lightness: 100,
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+      {
+        weight: 0.6,
+      },
+      {
+        saturation: -85,
+      },
+      {
+        lightness: 61,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+      {
+        color: '#5f94ff',
+      },
+      {
+        lightness: 26,
+      },
+      {
+        gamma: 5.86,
+      },
+    ],
+  },
+];
 function Map() {
   let map: google.maps.Map;
   const { isLoaded } = useLoadScript({
@@ -14,9 +133,10 @@ function Map() {
 
   const chineseAddr = '台灣台北市信義區吳興街220巷73弄5號';
   const getGeocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${chineseAddr}&key=${GoogleMapKey}`;
-  const keyword = '餐廳';
+  const keyword = '學校';
   const radius = 1000;
   const LatLng = { lat: 25.026221, lng: 121.560623 };
+
   const getRestaurantUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LatLng.lat},${LatLng.lng}&radius=${radius}&keyword=${keyword}&language=zh-TW&key=${GoogleMapKey}`;
   const [allRestaurants, setAllRestaurants] = useState<{ lat: number; lng: number }[]>();
   const addrs = useMemo(() => ({ lat: 25.026221, lng: 121.560623 }), []);
@@ -38,14 +158,17 @@ function Map() {
     // getGeocode();
 
     async function getRestaurant() {
-      await fetch(getRestaurantUrl).then((res) => {
+      await fetch(
+        `https://us-central1-rooroomies.cloudfunctions.net/googleMap?lat=${LatLng.lat}&lng=${LatLng.lng}&keyword=${keyword}`
+      ).then((res) => {
+        console.log(res);
         res.json().then((data) => {
-          const coordinates = new google.maps.LatLng({
-            lat: data.results[0].geometry.location.lat as number,
-            lng: data.results[0].geometry.location.lng as number,
+          console.log(data.results);
+          let allRestaurants: [] = [];
+          let allLatLng = data.results.map((p, index) => {
+            return p.geometry.location;
           });
-          console.log(data);
-          //   setAddrState(coordinates);
+          setAllRestaurants(allLatLng);
         });
       });
     }
@@ -54,8 +177,8 @@ function Map() {
   return (
     <>
       {isLoaded && (
-        <GoogleMap mapContainerStyle={mapStyles} zoom={14} center={addrs}>
-          <Marker position={addrs}></Marker>
+        <GoogleMap options={{ styles: style }} mapContainerStyle={mapStyles} zoom={14} center={addrs}>
+          {allRestaurants && allRestaurants.map((l, index) => <Marker position={l}></Marker>)}
         </GoogleMap>
       )}
     </>
